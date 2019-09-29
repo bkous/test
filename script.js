4946c88baf00bdb91ca430dc44c44b01d629ee49
bkous <boriskous@gmail.com>
2019-09-29 11:50:42 +0200

    nadaljevanje...

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("It works!"); 
+// console.log("It works!"); 
 
-let mainHeading = document.getElementsByTagName("h1");
-console.log(mainHeading);
+ let mainHeading = document.getElementsByTagName("h1");
+ console.log(mainHeading);
+
+let mainHeadingText = document.getElementsByTagName("h1")[0].textContent;
+
+console.log(mainHeadingText);
